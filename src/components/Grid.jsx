78ae590e05e345dcd27bf7897bc1a009aa77e0c2
Onge <onge.uk@gmail.com>
2018-01-08 21:40:38 +0000

    Add animations

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,14 +3,14 @@ import Tile from "./Tile";
 
 export default class Grid extends Component {
 	render() {
-		const {gridSize, gridData, tileInteraction} = this.props;
+		const {gridSize, gridData, tileInteraction, tilePulse} = this.props;
 
 		return (
 			<div class={`container grid-${gridSize}`}>
 				<div class="grid">
 					{gridData.map((item, i) => {
 						return (
-							<Tile id={i} value={item.value} colour={item.colour} angle={item.angle} flipped={item.flipped} tileInteraction={tileInteraction} />
+							<Tile id={i} value={item.value} colour={item.colour} angle={item.angle} flipped={item.flipped} pulse={item.pulse} tileInteraction={tileInteraction} tilePulse={tilePulse} />
 						);
 					})}
 				</div>
