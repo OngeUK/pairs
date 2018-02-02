564cffa9fd4c070954b7bfd93f6f71bebf1a7374
Onge <onge.uk@gmail.com>
2018-02-02 15:29:59 +0000

    Add favicons

--- src/entry.js
+++ src/entry.js
@@ -2,21 +2,16 @@
 import "./scss/app.scss";
 import {h, render} from "preact";
 
-// Import assets so these get bundled by Webpack
-// import "./../../assets/icon-logo.png";
-// import "./../../assets/banner-logo.png";
-// import "./manifest.json";
-// import "./sw.js";
-// import "./libs/workbox/workbox-sw.prod.v2.1.2.js";
-
 if (process.env.NODE_ENV !== "production") {
 	// When in development/serve, require HTML file
 	require("./index.html");
 } else {
 	// On build, require assets to get these bundled by Webpack
-	// import "./../../assets/icon-logo.png";
 	require("./images/banner.jpg");
 	require("./manifest.json");
+	require("./favicon.ico");
+	require("./favicon-16x16.png");
+	require("./favicon-32x32.png");
 	require("./sw.js");
 	require("./libs/workbox/workbox-sw.prod.v2.1.2.js");
 }
@@ -27,23 +22,11 @@ function init() {
 	let App = require("./components/app").default;
 
 	root = render(<App />, document.body, root);
-
-	// render(
-	// 	<div class="container">
-	// 		<App />
-	// 	</div>,
-	// 	document.body
-	// );
 }
-
-// // register ServiceWorker via OfflinePlugin, for prod only:
-// if (process.env.NODE_ENV==='production') {
-// 	require('./pwa');
-// }
 
 // in development, set up HMR:
 if (module.hot) {
-	require("preact/devtools"); // turn this on if you want to enable React DevTools!
+	require("preact/devtools"); // Enable React DevTools in Preact
 	module.hot.accept("./components/app", () => requestAnimationFrame(init));
 }
 
